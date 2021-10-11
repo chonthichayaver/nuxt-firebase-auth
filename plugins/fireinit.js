0f6e1d575ca8ff20bbb9e040bf5a6ad6c3f48ff1
chonyaver <fs.chonyaver@gmail.com>
2021-10-11 11:21:03 +0700

    ok

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -4,12 +4,14 @@ import 'firebase/auth'
 import 'firebase/database'
 
 var config = {
-  apiKey: 'AIzaSyAzdoAjlM9YlQ-gl8VRayCxtJbnrl9qDsw',
-  authDomain: 'nuxt-firebase-auth.firebaseapp.com',
-  databaseURL: 'https://nuxt-firebase-auth.firebaseio.com',
-  projectId: 'nuxt-firebase-auth',
-  storageBucket: 'nuxt-firebase-auth.appspot.com',
-  messagingSenderId: '316484287956'
+    apiKey: 'AIzaSyDcPP--N0QLnWH56rwhY8Ug1RG42q7-8E0',
+    authDomain: 'fir-nuxtjs-web-app.firebaseapp.com',
+    databaseURL: 'https://firebase-nuxtjs-web-app-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'firebase-nuxtjs-web-app',
+    storageBucket: 'firebase-nuxtjs-web-app.appspot.com',
+    messagingSenderId: '305492540148',
+    appId: '1:305492540148:web:dc284f61fcbdd2cb30bca1',
+    measurementId: 'G-V613BXG5CV'
 }
 
 if (!firebase.apps.length) firebase.initializeApp(config)
